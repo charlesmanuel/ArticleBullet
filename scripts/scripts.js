b25f80ac72875beb3dc03bf9e16451c0c9c94692
Amory Manuel <amory@umich.edu>
2020-02-02 17:38:31 -0500

    Opening bullet now changes expand icon to contract icon.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,11 @@
-function dropdown(id) {
+function dropdown(id, imgid) {
   if(document.getElementById(id).hidden == true){
     document.getElementById(id).hidden = false;
+    document.getElementById(id).style = "margin-top:8px";
+    document.getElementById(imgid).src="./svgs/icons/Box-arrow-up-left.svg";
   }
   else{
     document.getElementById(id).hidden = true;
+    document.getElementById(imgid).src="./svgs/icons/Box-arrow-bottom-right.svg";
   }
 }

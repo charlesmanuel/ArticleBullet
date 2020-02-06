a7f796f936c96396c4336f40c21c104c82d3fc4e
Amory Manuel <amory@umich.edu>
2020-02-05 20:41:47 -0500

    tweaking icon options

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,11 +2,10 @@ function dropdown(id, imgid) {
   if(document.getElementById(id).hidden == true){
     document.getElementById(id).hidden = false;
     document.getElementById(id).style = "margin-top:8px";
-    document.getElementById(imgid).src="./svgs/icons/Box-arrow-down.svg";
   }
   else{
     document.getElementById(id).hidden = true;
-    document.getElementById(imgid).src="./svgs/icons/Box-arrow-bottom-right.svg";
+    document.getElementById(imgid).src="./svgs/icons/document-text.svg";
   }
 }
 

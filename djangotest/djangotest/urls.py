7b0fb7be460e3423f2a3f68d25da5725f3348f70
Amory Manuel <amory@umich.edu>
2020-02-11 11:24:33 -0500

    additional django setup

--- djangotest/djangotest/urls.py
+++ djangotest/djangotest/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url
 from django.urls import include, path, re_path
+
 from boards import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', admin.site.urls),
+    path('', views.home, name='home'),
 ]

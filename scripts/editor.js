cfe5875827246da3728f7e9a16cc86c309beffdd
Amory Manuel <amory@umich.edu>
2020-03-16 15:34:06 -0400

    Added "add new bullet point" to editor functionality.

--- scripts/editor.js
+++ scripts/editor.js
@@ -7,12 +7,15 @@ $(document).ready(function(){
   });
 
   $('.minusbutton').click(function(){
+    console.log("minus successfully used");
     $(this).closest("li").remove();
   });
-
-  $( ".test2" ).keypress(function() {
-  console.log( "Handler for .keypress() called." );
-  });
+  $('.newpt2').click(function(){
+    //$("<li><textarea class='test2' rows='1' placeholder='Bullet 3'></textarea><div class='btncol' align='left'><button class='btn btn-danger minusbutton' href='#'>&#x2212;</button><button class='btn btn-primary addbutton' href='#'>&#x22ef;</button></div></li>").appendTo( "#ultext");
+    var nextpt = $( "#secretpt" ).clone(true);
+    $(nextpt).css("display", "list-item");
+    $("#ultext").append(nextpt);
+  })
 });
 
 
